1db5eb2b016895159a3aa4bd2c70aeed0e72cf64
Punit <puneethkumarvh@gmail.com>
2024-12-09 00:14:34 +0530

    change redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     {
       scope: 'userinfo.email,userinfo.profile,openid',
-      redirect_uri: 'https://employee-reimbursement-app.onrender.com//auth/google_oauth2/callback',
+      redirect_uri: 'https://employee-reimbursement-app.onrender.com/auth/google_oauth2/callback',
       prompt: 'select_account'
     }
 end
